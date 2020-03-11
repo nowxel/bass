caa5309da970496d06b0e5088af72ee82c3c7f95
nowxel <58902795+nowxel@users.noreply.github.com>
2020-03-11 23:12:46 +0200

    HomeWork
    remane file to hello.js, if you want to use it with index.html

---
+++ task6.js
@@ -0,0 +1,14 @@
+let birthYear = 1995;
+console.log(birthYear); // "number"
+let isMarried = confirm("Are you married? OK = Yes");
+console.log(isMarried); // "boolean"
+let firstName = "Max";
+console.log(typeof firstName); // "string"
+let lastName = "Zebola";
+console.log(typeof lastName); // "string"
+let group = "Lv-494.JS core";
+console.log(typeof group); // "string"
+let nullA = null;
+console.log(nullA); // "null"
+let age;
+console.log(age); // "undefined"
