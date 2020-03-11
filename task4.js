9a0368e740a7973aa92cbe0af7f482a06bf08e74
nowxel <58902795+nowxel@users.noreply.github.com>
2020-03-11 22:42:40 +0200

    HomeWork
    rename this file into hello.js, if you want to use it with index.html file

---
+++ task4.js
@@ -0,0 +1,14 @@
+let name = "Tom";
+console.log(typeof name); // "string"
+
+let income = 45.8;
+console.log(typeof income); // "number"
+
+let isEnabled = true;
+console.log(typeof isEnabled); // "boolean"
+
+let undefVariable;
+console.log(typeof undefVariable); // "undefined"
+
+let nullVariable = null;
+console.log(typeof nullVariable); // "object"
